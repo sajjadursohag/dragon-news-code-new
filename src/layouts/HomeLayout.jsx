6a999315630a94be7be512d40a38f97fe03a2483
sajjadursohag <sajjadursub07@gmail.com>
2026-08-15 23:33:28 +0600

    added components folder

--- src/layouts/HomeLayout.jsx
+++ src/layouts/HomeLayout.jsx
@@ -1,23 +1,36 @@
-import React from 'react'
-import { Outlet } from 'react-router'
-import Header from '../components/Header'
+import React from "react";
+import { Outlet } from "react-router";
+import Header from "../components/Header";
+import LatestNews from "../components/LatestNews";
+import Navbar from "../components/Navbar";
+import LeftAside from "../components/homelayout/LeftAside";
+import RightAside from "../components/homelayout/RightAside";
 
 const HomeLayout = () => {
   return (
     <div>
-          <header>
-              <Header></Header>
-          </header>
-          <main>
-              <section className="left_nav"></section>
-              <section className="main">
-                  <Outlet></Outlet>
-              </section>
-              <section className="right_nav"></section>
-          </main>
+      <header>
+        <Header></Header>
+        <section className="w-11/12 mx-auto my-3">
+          <LatestNews></LatestNews>
+        </section>
+        <nav className="w-11/12 mx-auto my-3">
+          <Navbar></Navbar>
+        </nav>
+      </header>
+      <main className="w-11/12 mx-auto my-3 grid grid-cols-12">
+        <aside className="col-span-3">
+          <LeftAside></LeftAside>
+        </aside>
+        <section className="main col-span-6">
+          <Outlet></Outlet>
+        </section>
+        <aside className="col-span-3">
+          <RightAside></RightAside>
+        </aside>
+      </main>
     </div>
-  )
-}
+  );
+};
 
-export default HomeLayout
-
+export default HomeLayout;
---
+++ src/routes/pages/CategoryNews.jsx
@@ -0,0 +1,14 @@
+import React from 'react'
+import { useParams } from 'react-router'
+
+const CategoryNews = () => {
+    const {id} = useParams();
+    console.log(id)
+  return (
+    <div>
+      Category News - {id}
+    </div>
+  )
+}
+
+export default CategoryNews
