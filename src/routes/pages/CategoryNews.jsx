05a1c1bdf5cbffa9d1fed75b6583406f5b5586ec
sajjadursohag <sajjadursub07@gmail.com>
2026-08-16 06:03:55 +0600

    added category news data filter

--- src/routes/pages/CategoryNews.jsx
+++ src/routes/pages/CategoryNews.jsx
@@ -1,14 +1,31 @@
-import React from 'react'
-import { useParams } from 'react-router'
+import React, { useEffect, useState } from "react";
+import { useLoaderData, useParams } from "react-router";
 
 const CategoryNews = () => {
-    const {id} = useParams();
-    console.log(id)
-  return (
-    <div>
-      Category News - {id}
-    </div>
-  )
-}
+  const { id } = useParams();
+  const data = useLoaderData();
 
-export default CategoryNews
+  // ui te dekhanor jonno ekta state declare korte hobe
+  const [categoryNews, setCategoryNews] = useState([]);
+
+  // console.log(id, data);
+  useEffect(() => {
+    if (id == "0") {
+      setCategoryNews(data);
+      return;
+    } else if (id == "1") {
+      const filterdNews = data.filter(
+        (news) => news.others.is_today_pick == true,
+      );
+      setCategoryNews(filterdNews);
+    } else {
+      const filterdNews = data.filter((news) => news.category_id == id);
+      console.log(filterdNews);
+
+      setCategoryNews(filterdNews);
+    }
+  }, [data, id]);
+  return <div>Total {categoryNews.length} Found</div>;
+};
+
+export default CategoryNews;
